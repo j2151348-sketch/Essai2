b1fc916dde51c82e74edfeb4f403b9990153609d
j2151348-sketch <j2151348@gmail.com>
2025-10-17 01:31:00 +0200

    Delete functions directory

--- functions/hello.js
+++
@@ -1,5 +0,0 @@
-export async function onRequestGet(context) {
-  return new Response(JSON.stringify({ message: "Fonctions Cloudflare actives ✅" }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
