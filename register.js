da64c9bcd9d47f53e290b46ea07176aa4d90fc90
j2151348-sketch <j2151348@gmail.com>
2025-10-17 01:31:27 +0200

    Delete register.js

--- register.js
+++
@@ -1,6 +0,0 @@
-export const onRequestPost = async ({ request }) => {
-  const data = await request.json().catch(()=>({}));
-  return new Response(JSON.stringify({ ok: true, received: data || null, note: 'Route /auth/register OK (démo).' }), {
-    headers: { 'content-type': 'application/json' }
-  });
-};
